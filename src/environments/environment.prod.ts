e18c3cc5dfd516f6830578723eadb56b4625f1bf
bhamoud@icloud.com <bhamoud@icloud.com>
2023-01-15 03:18:24 +0100

    initial commit
    Signed-off-by: bhamoud@icloud.com <bhamoud@icloud.com>

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'shopping-list-2dfa7',
+    appId: '1:398556809132:web:80993496a27e1dca4e1c09',
+    storageBucket: 'shopping-list-2dfa7.appspot.com',
+    apiKey: 'AIzaSyCKdQZEZqQp3ZIjUzrXdod-x7I9WW8MzfY',
+    authDomain: 'shopping-list-2dfa7.firebaseapp.com',
+    messagingSenderId: '398556809132',
+  },
   production: true
 };
